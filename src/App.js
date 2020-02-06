99b9e535f1fa3d82d016ccc57fd2d28eb4ef5b3d
Ryan Carville <ryancarville@gmail.com>
2020-02-06 18:34:04 +0000

    placehold ready

--- src/App.js
+++ src/App.js
@@ -11,14 +11,20 @@ function App() {
       <article className="welcome-content">
         <h2>Welcome to Calabash Villa</h2>
         <p>
-          We are currently in the process of building the platform, but you can
-          view the current state of the villa via our Instagram account{" "}
+          We are currently in the process of building the platform as well as
+          the villa! <br />
+          <br />
+          You can view the current state of the villa via our Instagram account{" "}
           <a
             href="https://www.instagram.com/calabashvillabequia"
             rel="noopener noreferrer"
             target="_blank"
           >
             here
+          </a>{" "}
+          or you can email us at{" "}
+          <a href="mailto:hello@calabashvillabequia.com?subject=Inquiry from website">
+            hello@calabashvillabequia.com
           </a>
         </p>
       </article>
