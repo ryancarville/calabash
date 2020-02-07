0088a6a4427752469b4be57cac921ba7fedede65
Ryan Carville <ryancarville@gmail.com>
2020-02-07 15:05:03 +0000

    mobiel ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -7,20 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="/images/calabash_logo_social.jpg" alt="logo" />
+        <figure className="bg-img" />
       </header>
       <article className="welcome-content">
-        <h2>Welcome to Calabash Villa</h2>
         <p>
           We are currently in the process of building the platform as well as
           the villa! <br />
           <br />
-          You can view the current state of the villa via our Instagram account{" "}
+          You can view the current state of the villa via our{" "}
           <a
             href="https://www.instagram.com/calabashvillabequia"
             rel="noopener noreferrer"
             target="_blank"
           >
-            here
+            Instagram account
           </a>{" "}
           or you can email us at{" "}
           <a href="mailto:hello@calabashvillabequia.com?subject=Inquiry from website">
