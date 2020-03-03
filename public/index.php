33e2c8a4bc1f0139705690bb626f108b6b96e432
Ryan Carville <ryancarville@gmail.com>
2020-03-03 11:14:42 -0400

    added post header

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ $allowedMethods = array(
     'POST'
 );
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 $rest_json = file_get_contents("php://input");
