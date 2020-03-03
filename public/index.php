7580b5ca03bcb939dfadeba8865964dde9c6699e
Ryan Carville <ryancarville@gmail.com>
2020-03-03 12:35:12 -0400

    form

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Origin: *");
+
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
+echo $_POST;
 if ($_POST)
     {
     // set response code - 200 OK
